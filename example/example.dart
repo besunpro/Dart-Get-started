7356c218e67732a478fbd1e832adf85f9d151af9
besunpro <be.sunpro@gmail.com>
2013-09-23 02:48:15 -0400

    initial version

---
+++ example/example.dart
@@ -0,0 +1,14 @@
+import 'package:ex03lib/ex03lib.dart';
+
+main(){
+  
+  num a1=50;  
+  num a2=10;
+  
+  print('Example of using Ex03 library');
+  print('');
+  print('- Produit de $a1 et $a2: ${product(a1,a2)}'); 
+  print(''); 
+  print('- Quotient de $a1 et $a2: ${quotient(a1,a2)}');
+  
+}
